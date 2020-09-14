8b5297ad9865dfd7938e5d58a4b9ecc3e53d8fc5
pupil <pupil@ivkhk.ee>
2020-09-14 14:30:01 +0300

    Ночальный коммит

---
+++ src/jjktvr19myarray/JJKTVR19MyArray.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jjktvr19myarray;
+
+/**
+ *
+ * @author pupil
+ */
+public class JJKTVR19MyArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
